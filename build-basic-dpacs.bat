91cf6e2dac7f328c61f9099bb414ed5564305253
Rob Ratcliff <rob@futuretek.com>
2022-12-14 14:18:54 -0600

    Update for dpacs 2.39.84 Use 2.39.20 for DPACS version of RaptorX with local mods Fix error in gradle script that was pointing to the wrong version of postgresql and hypersonic plugins.

--- build-basic-dpacs.bat
+++ build-basic-dpacs.bat
@@ -1,3 +1,5 @@
+set JAVA_HOME=c:\java\jdk1.8
+set PATH=%JAVA_HOME%\bin;%PATH%
 gradlew -b build-basic-dpacs.gradle clean build makeDpacsInstallers makeAllDpacsBundles
 rem gradlew -b build-basic-dpacs.gradle clean build makeSeraphInstallers makeAllSeraphBundles
 pause -1
