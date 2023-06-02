00d0870abab53c0ed68d0604a9211fe13036409c
robert.ratcliff <rob@futuretek.com>
2023-06-02 17:08:24 -0500

    setup for the Intrepid and the static base configuration

--- build-basic-dpacs.bat
+++ build-basic-dpacs.bat
@@ -1,4 +1,4 @@
-set JAVA_HOME=c:\java\jdk1.8
+set JAVA_HOME=c:\java\jdk-1.8
 set PATH=%JAVA_HOME%\bin;%PATH%
 gradlew -b build-basic-dpacs.gradle clean build makeDpacsInstallers makeAllDpacsBundles
 rem gradlew -b build-basic-dpacs.gradle clean build makeSeraphInstallers makeAllSeraphBundles
--- build.bat
+++ build.bat
@@ -1,3 +1,5 @@
+set JAVA_HOME=c:\java\jdk-1.8
+set PATH=%JAVA_HOME%\bin;%PATH%
 gradlew clean build makeSeraphInstallers makeAllSeraphBundles
 rem gradlew makeSeraphInstallers makeAllSeraphBundles
 pause
