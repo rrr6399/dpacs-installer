d62668630d1bfe75d6aeccbefb2e9ab713658a6e
robert.ratcliff <rob@futuretek.com>
2023-10-11 23:23:29 -0500

    follow me settings and 2.39.102

--- build-basic-dpacs.bat
+++ build-basic-dpacs.bat
@@ -2,4 +2,4 @@ set JAVA_HOME=c:\java\jdk-1.8
 set PATH=%JAVA_HOME%\bin;%PATH%
 gradlew -b build-basic-dpacs.gradle clean build makeDpacsInstallers makeAllDpacsBundles
 rem gradlew -b build-basic-dpacs.gradle clean build makeSeraphInstallers makeAllSeraphBundles
-pause -1
+pause 
